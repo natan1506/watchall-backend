5d8e0d61e9663e8e3cd2d2a1178eefacd57f2479
Natan Souza <42328139+natan1506@users.noreply.github.com>
2024-11-10 10:23:22 -0300

    fix: CORS

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
-import { app } from './app';
-import { env } from './env';
-import cors from '@fastify/cors'
-import { userRoutes } from './routes/user';
-import { moviesRoutes } from './routes/movies';
+import { app } from "./app";
+import { env } from "./env";
+import cors from "@fastify/cors";
+import { userRoutes } from "./routes/user";
+import { moviesRoutes } from "./routes/movies";
 
-app.register(userRoutes)
-app.register(moviesRoutes)
+app.register(userRoutes);
+app.register(moviesRoutes);
 
 app.register(cors, {
-  origin: 'https://watchall.vercel.app/*', // Permitir seu frontend específico
+  origin: env.NODE_ENV === "dev" ? env.NODE_ORIGIN : `${env.NODE_ORIGIN}/*`, // Permitir seu frontend específico
 });
 
-
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT,
-}).then(() => {
-  console.log(`🚀 Server Running! PORT: ${env.PORT} ${env.NODE_ORIGIN}`)
-})
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Server Running! PORT: ${env.PORT} ${env.NODE_ORIGIN}`);
+  });
